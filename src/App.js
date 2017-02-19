fcf194854b054d38a9e652f7421188406dab59be
cyruslognonne <cyrus7@hotmail.fr>
2017-02-18 23:22:02 -0500

    Adding the typewritter

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import FixedImgs from './FixedImgs.js'
 import Home from "./Home.js"
 import FixedComponents1 from "./FixedComponents1.js";
 import FixedComponents2 from "./FixedComponents2.js";
-import FixedComponents3 from "./FixedComponents3.js"
-
+import FixedComponents3 from "./FixedComponents3.js";
+import TypeWriter from 'react-typewriter';
 
 
 
@@ -15,12 +15,15 @@ class App extends Component {
     render() {
         return(
             <div className="full-page">
+
               <NavBar></NavBar>
               <FixedImgs></FixedImgs>
               <FixedComponents1></FixedComponents1>
-              <FixedComponents2></FixedComponents2>
               <FixedComponents3></FixedComponents3>
 
+                <TypeWriter className="typeWritter" minDelay={75} typing={1}>
+                  '_“If you are irritated by every rub, how will your mirror be polished?” Sufi Master'
+                </TypeWriter>
 
               <Home></Home>
             </div>
