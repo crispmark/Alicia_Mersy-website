9cba57026c77e1417de15c59f97d97ca8f520de5
cyruslognonne <cyrus7@hotmail.fr>
2017-06-04 22:45:04 +0300

    Updating the sticky

--- src/FirstBackground.js
+++ src/FirstBackground.js
@@ -18,15 +18,13 @@ return (
     {
       [1,2,3,4,5,6,7,8].map(i => {
         return (
-          <StickyContainer key={i} style={{ height: 1000, background: `hsl(${i*40}, 70%, 90%)` }}>
+          <StickyContainer key={i} style={{ height: 1000}}>
             <Sticky>
               {
                 ({ isSticky, wasSticky, style, distanceFromTop, distanceFromBottom, calculatedHeight }) => {
                   return (
-                    <div style={{ ...style, height: 80, background: `hsl(${i*40}, 70%, 50%)`}}>
-                      <h2 style={{ lineHeight: '80px', padding: '0 15px', margin: 0 }}>
-                        <span className="pull-left">&lt;Sticky #{i} /&gt;</span>
-                      </h2>
+                    <div>
+                      <img className="sticky_img" src="https://res.cloudinary.com/www-c-t-l-k-com/image/upload/c_scale,w_1900/v1487026129/background_11_x5ob78.jpg" />
                     </div>
                   )
                 }
